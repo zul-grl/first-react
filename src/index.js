ac1e3ee89992026592fc31cf19c1ea563ab1492c
24LP7683 24LP7683 <24LP7683@G304-CC0225.local>
2024-12-16 12:06:21 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { react } from "@babel/types";
+import App from "./Home";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
